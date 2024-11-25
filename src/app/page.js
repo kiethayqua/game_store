19500894794e70fd92e6ce52450fdfa7e88f9d7c
kiethayqua <kiethayqua@gmail.com>
2024-11-25 20:01:00 +0700

    feat: init

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,157 @@
+"use client"
+import React, { useEffect, useState } from "react";
+import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Pagination, Stack, Toolbar, Typography } from "@mui/material";
+import InfoIcon from '@mui/icons-material/Info';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import page1 from '../mock/page1.json';
+import page2 from '../mock/page2.json';
+import page3 from '../mock/page3.json';
+import page4 from '../mock/page4.json';
+import page5 from '../mock/page5.json';
 import Image from "next/image";
-import styles from "./page.module.css";
+
+const pages = ['Liên hệ'];
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [popupData, setPopupData] = useState(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const getGames = async (page = 1) => {
+    setLoading(true);
+    let data = null;
+    switch (page) {
+      case 1:
+        data = page1;
+        break;
+      case 2:
+        data = page2;
+        break;
+      case 3:
+        data = page3;
+        break;
+      case 4:
+        data = page4;
+        break;
+      case 5:
+        data = page5;
+        break;
+    }
+    setData(data.results);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getGames();
+  }, []);
+
+  const onCloseModal = () => {
+    setPopupData(null);
+  }
+
+  const onDetail = (game) => {
+    setPopupData(game);
+  }
+
+  const getRequirementData = (platforms) => {
+    const found = platforms?.find(p => p?.requirements_en != null && p?.requirements_en?.recommended);
+    return found?.requirements_en?.recommended || '';
+  }
+
+  return (
+    <Container sx={{ background: 'white', paddingBottom: 4 }}>
+      <AppBar sx={{ background: 'rgb(16, 16, 20)' }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Stack flexDirection={'row'} alignItems={'center'}>
+              <SportsEsportsIcon />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.1rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                CHỢ GAME
+              </Typography>
+            </Stack>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Container sx={{ paddingTop: 12, background: 'white' }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {data.map((game, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 160 }}
+                  image={game.background_image}
+                  title=""
+                />
+                <CardContent>
+                  <Typography gutterBottom variant='body1' fontWeight={700}>
+                    {game.name}
+                  </Typography>
+                  <Typography gutterBottom variant='body1'>
+                    {'499.000 VND'}
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ justifyContent: 'flex-end' }}>
+                  <Button startIcon={<InfoIcon />} variant="outlined" size="small" onClick={() => { onDetail(game) }}>Chi Tiết</Button>
+                  <Button startIcon={<ShoppingCartIcon />} variant="contained" size="small">Mua Game</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Stack direction='row' sx={{
+        marginTop: 4,
+        justifyContent: 'center',
+      }}>
+        <Pagination
+          count={5}
+          onChange={(_, page) => {
+            getGames(page);
+          }}
+          color="primary"
+          variant="outlined"
+          shape="rounded"
+        />
+      </Stack>
+
+      <Dialog onClose={onCloseModal} open={popupData} fullWidth>
+        <DialogTitle>{popupData?.name}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <div style={{ paddingLeft: 16, paddingRight: 16 }} dangerouslySetInnerHTML={{ __html: getRequirementData(popupData?.platforms) }}>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="text" size="small" onClick={onCloseModal}>Đóng</Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
   );
 }
